12260e77a18767229173d4fe85e14cd933767c35
hye-inA <hye.ina0130@gmail.com>
2026-01-05 11:27:25 +0900

    feat : 이메일 발송 서비스 코드 추가

--- notify-practice1/src/main/java/com/ses/messaging/service/OrderService.java
+++ notify-practice1/src/main/java/com/ses/messaging/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.ses.messaging.service;
 
+import com.ses.messaging.dto.CreateOrderRequest;
+import com.ses.messaging.dto.CreateOrderResponse;
 import com.ses.messaging.dto.Item;
 import com.ses.messaging.entity.ShopData;
 import com.ses.messaging.repository.ShopDataRepository;
@@ -18,14 +20,18 @@ public class OrderService {
 
 
     private final ShopDataRepository repository;
+    private final EmailService emailService;
 
     private static final String ITEM_PREFIX = "ITEM#";
 
-    public String createOrder(List<Item> items) {
+    public CreateOrderResponse createOrder(CreateOrderRequest request) {
         // 주문 ID 생성 (공유 PK)
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
         String uuid = UUID.randomUUID().toString().substring(0, 4);  // 첫 4글자만 사용
         String orderId = "ORD_" + timestamp + "_" + uuid;
+
+        // 주문 상품 목록
+        List<Item> items = request.items();
 
         // 주문 총액
         Long totalAmount = items.stream()
@@ -42,6 +48,7 @@ public class OrderService {
                 .type("ORDER")
                 .info("ORDER_CREATED")
                 .amount(totalAmount)
+                .email(request.email())
                 .build();
         shopDataList.add(orderInfo);
 
@@ -54,6 +61,7 @@ public class OrderService {
                     .type("ITEM")
                     .info(item.name())
                     .amount(item.price())
+                    .email(request.email())
                     .build();
             shopDataList.add(itemEntity);
         }
@@ -61,7 +69,17 @@ public class OrderService {
         // 트랜잭션을 위해서 한 번에 전달
         repository.save(shopDataList);
 
-        // 주문 ID 반환
-        return orderId;
+        // 이메일 발송 데이터 준비
+        String emailSubject = "[주문 완료] 주문이 성공적으로 접수되었습니다.";
+        String emailBody = String.format(
+                "고객님, 주문해주셔서 감사합니다.\n\n주문번호: %s\n결제금액: %,d원",
+                orderId, totalAmount
+        );
+
+        // 이메일 발송
+        emailService.sendEmail(request.email(), emailSubject, emailBody);
+
+        // 결과 DTO 반환
+        return new CreateOrderResponse(orderId, totalAmount, "주문 완료! 이메일을 확인하세요!");
     }
 }
